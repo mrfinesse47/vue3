f4c43ab97c7b57957f6693be61acb54496e10572
Kevin Mason <krmason2020@gmail.com>
2023-07-11 12:48:07 -0700

    11

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,9 +1,44 @@
 <template>
-  <div class="block">Click Me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
 </template>
 <script>
 export default {
   props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    // a lifecycle hook
+    // https://vuejs.org/guide/essentials/lifecycle.html#lifecycle-diagram
+    console.log('component mounted');
+    // start timer
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+      console.log(this.delay);
+    }, this.delay);
+  },
+  updated() {
+    console.log('updated this.showBlock due to updated hook');
+  },
+  unmounted() {
+    console.log('Block unmounted');
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log('reaction time', this.reactionTime);
+    },
+  },
 };
 </script>
 <style>
