252d3e908ae06cd9523824fe859cba627a7862a9
Kevin Mason <krmason2020@gmail.com>
2023-07-11 10:30:38 -0700

    .

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -4,9 +4,6 @@
     <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
   </div>
   <div>
-    <!-- <button @click.right="toggleModal">Open Modal</button>
-    an example of a click modifies .right will trigger the event on a right mouse click -->
-    <!-- <button @click.shift="toggleModal">Open Modal</button> -->
     <button @click.alt="toggleModal">Open Modal (alt/option)</button>
   </div>
 </template>
