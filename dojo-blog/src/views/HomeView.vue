edef265c9c84eddd055410d4fdf553b9aa4a6cc1
Kevin Mason <krmason2020@gmail.com>
2023-07-12 14:27:05 -0700

    14
    :34

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    home
+
+    <p ref="p">My Name is: {{ name }}</p>
+    <p>And my age is: {{ age }}</p>
+
+    <button @click="handelClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    let name = 'Mario'
+    // important: these are not reactive values, so if they change the dom wont update
+    let age = 30
+
+    //  a refs example, we need to import the ref func
+
+    const p = ref(null)
+
+    const handelClick = () => {
+      console.log(p)
+    }
+    return { name, age, handelClick, p }
+  },
+}
+</script>
