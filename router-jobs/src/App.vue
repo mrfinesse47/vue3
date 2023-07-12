e860d4adedc8a357ccbf06ce7a9dad5a323bc4b7
Kevin Mason <krmason2020@gmail.com>
2023-07-12 11:39:26 -0700

    folder-structure

--- router-jobs/src/App.vue
+++ router-jobs/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link :to="{ name: 'about' }">About</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link> |
+    <router-link :to="{ name: 'jobs' }">Jobs</router-link>
     <!-- can databind as well, the benefit to this is if the routes are dynamic-->
   </nav>
   <router-view />
