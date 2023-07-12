5433cf7680b3dc2df8800a4d1657ddf4404f29fa
Kevin Mason <krmason2020@gmail.com>
2023-07-12 13:58:06 -0700

    .

--- router-jobs/src/views/jobs/JobDetailsView.vue
+++ router-jobs/src/views/jobs/JobDetailsView.vue
@@ -1,12 +1,26 @@
 <template>
-  <h1>Jobs Details Page</h1>
-  <p>The Job ID is {{ id }}</p>
-  <!-- the $route contains any information about the current route -->
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The Job ID is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details.....</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ['id'],
+  data() {
+    return { job: null };
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
