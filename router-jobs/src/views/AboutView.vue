f87033639103d942bf0c4ad12721503cef60c1ba
Kevin Mason <krmason2020@gmail.com>
2023-07-12 11:23:12 -0700

    begin routing

---
+++ router-jobs/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+      nesciunt magnam cumque, consectetur eius itaque delectus laborum voluptas?
+      Fuga, dicta blanditiis. Deserunt dolorem reprehenderit error obcaecati
+      ipsam repellat in enim asperiores eveniet? Excepturi fugiat nemo maxime ut
+      consectetur, in ratione repellat sint quis quos, voluptatem reprehenderit
+      cupiditate est aperiam beatae minus distinctio repellendus delectus
+      tempora. Esse id culpa laborum consequuntur cupiditate, reiciendis
+      necessitatibus, expedita numquam ullam deserunt explicabo adipisci. Ab
+      architecto nisi voluptates esse commodi quisquam maiores reprehenderit
+      magni! Totam eaque fuga harum sequi blanditiis ratione? Repellat ex ullam,
+      illum quia accusantium cumque illo laborum dolore quidem rem, repellendus
+      fugit.
+    </p>
+  </div>
+</template>
---
+++ router-jobs/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eaque ullam
+      repellendus, alias natus a at cum minima officia aspernatur minus in
+      fugiat, ad, numquam vero. Totam, iste dolor et a dolorum temporibus eum
+      similique culpa, quo quasi error vero provident? Architecto incidunt illum
+      harum illo est rem sunt nisi suscipit aliquid dignissimos voluptatum
+      facere voluptatem ducimus adipisci voluptas, sequi inventore voluptatibus
+      deserunt doloremque expedita accusamus! Praesentium neque doloribus
+      officiis natus quo perspiciatis assumenda quidem ad error modi cupiditate
+      consequatur repellendus unde suscipit quam porro eum dolor sint aperiam
+      ut, fugiat at! Vel architecto voluptatum, voluptates quidem laborum eaque
+      quasi.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {},
+};
+</script>
