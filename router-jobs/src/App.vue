91befc3e5616618336f8590cfd37537bee85e17f
Kevin Mason <krmason2020@gmail.com>
2023-07-12 12:56:42 -0700

    history

--- router-jobs/src/App.vue
+++ router-jobs/src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <nav>
+  <nav id="nav">
     <router-link to="/">Home</router-link> |
     <router-link :to="{ name: 'about' }">About</router-link> |
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
     <!-- can databind as well, the benefit to this is if the routes are dynamic-->
   </nav>
+
+  <button @click="redirect">redirect user</button>
+  <button @click="back">go back a page</button>
+  <button @click="forward">go forward a page</button>
+
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'home' });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -15,13 +36,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 960px;
+  margin: 0 auto;
 }
 
-nav {
+#nav {
   padding: 30px;
 }
 
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
   text-decoration: none;
@@ -29,8 +52,15 @@ nav a {
   border-radius: 4px;
 }
 
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
