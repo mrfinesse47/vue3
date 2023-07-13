589a814a444e00b2d8b81c29a82e92929c011377
Kevin Mason <krmason2020@gmail.com>
2023-07-13 12:14:16 -0700

    .

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <PostList :posts="posts" v-if="showPosts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">Delete a post</button>
   </div>
 </template>
 
@@ -17,7 +19,8 @@ export default {
       { title: 'welcome to the blog', body: 'lorem ipsum1', id: 1 },
       { title: 'top 5 css tips', body: 'lorem ipsum2', id: 2 },
     ])
-    return { posts }
+    const showPosts = ref(true)
+    return { posts, showPosts }
   },
 }
 </script>
