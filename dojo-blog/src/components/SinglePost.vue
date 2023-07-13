e775d2947ddb6abbece892ffca1c6f46694d90c6
Kevin Mason <krmason2020@gmail.com>
2023-07-13 11:38:47 -0700

    .'

---
+++ dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>
+      {{ snippet }}
+    </h3>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  setup(props) {
+    const snippet = computed(() => props.post.body.substring(0, 3) + '....')
+    return { snippet }
+  },
+  props: ['post'],
+}
+</script>
+
+<style></style>
--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,49 +1,23 @@
 <template>
   <div class="home">
-    <h1>home</h1>
-    <input type="text" v-model="search" />
-    <p>search term : {{ search }}</p>
-
-    <div v-for="name in names" :key="name">
-      <p>{{ name }}</p>
-    </div>
-    <h3>matching names</h3>
-    <div v-for="name in matchingNames" :key="name">
-      <p>{{ name }}</p>
-    </div>
-    <button @click="stopWatching">stop watching</button>
+    <h1>Home</h1>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', , 'wario', 'rex', 'yoshi', 'natalie'])
-    const stopWatch = watch(search, () => {
-      console.log('watch fx ran')
-    })
-    // // watchefect only runs initially
-    // watchEffect(() => {
-    //   console.log('watch effect function ran')
-    // })
-    // but if you use a ref it will, so it runs everytime search changes
-    // it watches any dependancies
-    const stopEffect = watchEffect(() => {
-      // useful for grabbing data like useeffect in react
-      console.log('watch effect function ran', search.value)
-    })
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-    const stopWatching = () => {
-      stopWatch()
-      stopEffect()
-    }
-    return { names, search, matchingNames, stopWatching }
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'lorem ipsum1', id: 1 },
+      { title: 'top 5 css tips', body: 'lorem ipsum2', id: 2 },
+    ])
+    return { posts }
   },
 }
 </script>
