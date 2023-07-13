1ee51364558de6aa953ea68d2f8932dd8a5d9eed
Kevin Mason <krmason2020@gmail.com>
2023-07-13 11:01:33 -0700

    .

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,38 +1,31 @@
-<!--  @ 14:25 -->
-
 <template>
   <div class="home">
-    home
+    <h1>home</h1>
+    <input type="text" v-model="search" />
+    <p>search term : {{ search }}</p>
 
-    <p ref="p">My Name is: {{ name }}</p>
-    <p>And my age is: {{ age }}</p>
-    <button @click="handelClick">Click Me</button>
-    <button @click="age++">Add one to age</button>
-    <input type="text" v-model="name" />
-    <!-- outside in the template we dont need .value -->
+    <div v-for="name in names" :key="name">
+      <p>{{ name }}</p>
+    </div>
+    <h3>matching names</h3>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
-    // we have only used refs to get a template ref so far
-
-    const name = ref('Mario')
-    // // when we use ref to surround a value it becomes reactive
-    // refs are pointers so we can assign to const
-
-    const age = ref(30)
-
-    const handelClick = () => {
-      // name = 'luigi'  fails to update the name in the template
-      name.value = 'luigi'
-      age.value = 36
-    }
-    return { name, age, handelClick }
+    const search = ref('')
+    const names = ref(['mario', , 'wario', 'rex', 'yoshi', 'natalie'])
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+    return { names, search, matchingNames }
   },
 }
 </script>
